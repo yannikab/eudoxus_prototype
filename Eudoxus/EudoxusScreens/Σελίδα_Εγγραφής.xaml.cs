336572a63a2719916df53abe9de52add16d0d04c
Yanni Kabilafkas <yanni.kab@gmail.com>
2015-09-19 01:25:36 +0300

    Eudoxus prototype.

---
+++ Eudoxus/EudoxusScreens/Σελίδα_Εγγραφής.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EudoxusScreens
+{
+	public partial class Σελίδα_Εγγραφής : UserControl
+	{
+		public Σελίδα_Εγγραφής()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
---
+++ Eudoxus/EudoxusScreens/Σελίδα_Επιλογών_Εκδότη.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace EudoxusScreens
+{
+	public partial class Σελίδα_Επιλογών_Εκδότη : UserControl
+	{
+		public Σελίδα_Επιλογών_Εκδότη()
+		{
+			// Required to initialize variables
+			InitializeComponent();
+		}
+	}
+}
